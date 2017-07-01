0754f3b0ab83e80ac412a175c808deedee33b727
Thomas Smith <thom@Thomass-iMac.lan>
2017-07-01 16:15:49 -0400

    Switched YAML engine to ruamel.

--- src/build.py
+++ src/build.py
@@ -1,10 +1,11 @@
-import yaml
+# import yaml
 from os import path
 import sys
 import imp
 from inspect import signature, Parameter
+from io import StringIO
 
-from .yamlordereddictloader import yamlordereddictloader
+import ruamel.yaml
 
 def load_macros(macro_path):
     search_path, name = path.split(path.abspath(macro_path))
@@ -20,11 +21,11 @@ def load_macros(macro_path):
 
 def apply_transformation(loader, node, transform):
     try:
-        if isinstance(node, yaml.ScalarNode):
+        if isinstance(node, ruamel.yaml.ScalarNode):
             return transform(loader.construct_scalar(node))
-        elif isinstance(node, yaml.SequenceNode):
+        elif isinstance(node, ruamel.yaml.SequenceNode):
             return transform(*loader.construct_sequence(node))
-        elif isinstance(node, yaml.MappingNode):
+        elif isinstance(node, ruamel.yaml.MappingNode):
             if any(
                 param.kind == Parameter.VAR_POSITIONAL
                 for name, param in signature(transform).parameters.items()
@@ -39,21 +40,22 @@ def get_constructor(transform):
     return lambda loader, node: apply_transformation(loader, node, transform)
 
 def build_yaml_macros(input, macros_search_path):
-    for token in yaml.scan(input):
-        if isinstance(token, yaml.tokens.DocumentStartToken):
+    yaml = ruamel.yaml.YAML()
+    yaml.version = (1,2)
+
+    for token in ruamel.yaml.scan(input):
+        if isinstance(token, ruamel.yaml.tokens.DocumentStartToken):
             break
-        elif isinstance(token, yaml.tokens.DirectiveToken) and token.name == 'TAG':
+        elif isinstance(token, ruamel.yaml.tokens.DirectiveToken) and token.name == 'TAG':
             handle, prefix = token.value
             if not prefix.startswith('tag:yaml-macros:'): break
             macro_path = path.join(macros_search_path, prefix.split(':')[2])
             for name, transform in load_macros(macro_path):
-                yaml.add_constructor(prefix+name, get_constructor(transform))
+                # yaml.constructor.add_constructor(prefix+name, get_constructor(transform))
+                yaml.constructor.add_constructor(handle+name, get_constructor(transform))
 
-    syntax = yaml.load(input, Loader=yamlordereddictloader.Loader)
+    syntax = yaml.load(input)
 
-    return yaml.dump(syntax,
-        version=(1,2),
-        default_flow_style=False,
-        tags=False,
-        Dumper=yamlordereddictloader.Dumper
-    )
+    output = StringIO()
+    yaml.dump(syntax, stream=output)
+    return output.getvalue()
