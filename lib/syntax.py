69ee0590dff1b6ab34a51c282ae9dce348e90419
thom <thom@localhost.localdomain>
2017-08-11 16:34:31 -0400

    Added third argument to !expect.

--- lib/syntax.py
+++ lib/syntax.py
@@ -4,11 +4,18 @@ def meta(scope):
         { "match": r'', "pop": True },
     ]
 
-def expect(expr, scope):
-    return [
-        { "match": expr, "scope": scope, "pop": True },
+def expect(expr, scope, set_context=None):
+    ret = [
+        { "match": expr, "scope": scope },
         { "match": r'(?=\S)', "pop": True },
     ]
+
+    if set_context:
+        ret[0]['set'] = set_context
+    else:
+        ret[0]['pop'] = True
+
+    return ret
 
 def pop_on(expr):
     return {
